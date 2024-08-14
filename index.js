0ed1ac1116a335fdc5d350394f6f281842526c49
张峻豪 <111368815+The-Aries@users.noreply.github.com>
2024-08-14 17:59:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(function(request, response) {
     response.end("<html><body><h1>Hello World!</h1></body></html>");
 });
 
-const port = process.env.PORT || 1337;
+const port = 1337;
 server.listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
